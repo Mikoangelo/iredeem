b1abca12a702939cf2d5910617e7959e5b8fdb37
Mikael Høilund <mikael@hoilund.org>
2009-01-08 16:33:00 +0100

    Created visual part of interface.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iRedeem
+//
+//  Created by Mikael H¿ilund on 8/1/09.
+//  Copyright Meta.io 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
